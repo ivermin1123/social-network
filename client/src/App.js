c9f12711485b44c8dd8bee8a9911bb85953a9ba2
tvn0122 <ntat0013@gmail.com>
2020-11-02 15:43:46 +0700

    :zap: Add Provider, Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import {
+  Redirect,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route path="/404" component={NotFound} />
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotFound() {
+  return <h1>This page is not found</h1>;
+}
+
+export default NotFound;

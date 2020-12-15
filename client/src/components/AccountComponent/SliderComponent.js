e96503ed3f0acca315f92cf3f5792da18fb88436
trinhhoangyen <thienanh156@gmail.com>
2020-12-15 15:21:10 +0700

    Add SliderComponent

---
+++ client/src/components/AccountComponent/SliderComponent.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import sprite from "../../assets/icons/sprite.svg";
+import mainPic1 from "../../assets/image/main-pic-1.png";
+import mainPic2 from "../../assets/image/main-pic-2.jpg";
+import mainPic3 from "../../assets/image/main-pic-3.jpg";
+import mainPic4 from "../../assets/image/main-pic-4.jpg";
+import league from "../../assets/image/league-of-legends.png";
+
+const SliderComponent = () => {
+	const [nav1, setNav1] = useState(null);
+	const [nav2, setNav2] = useState(null);
+	let slider1 = [];
+	let slider2 = [];
+	useEffect(() => {
+		setNav1(slider1);
+		setNav2(slider2);
+	});
+	const list = [
+		{
+			image: mainPic1,
+			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
+			text: "League of Legends®",
+		},
+		{
+			image: mainPic2,
+			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
+			text: "League of Legends®",
+		},
+		{
+			image: mainPic3,
+			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
+			text: "League of Legends®",
+		},
+		{
+			image: mainPic4,
+			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
+			text: "League of Legends®",
+		},
+		{
+			image: mainPic2,
+			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
+			text: "League of Legends®",
+		},
+	];
+	const ButtonArrow = (props) => {
+		const { className, onClick, icon } = props;
+		return (
+			<button
+				type="button"
+				className={`${className} slick-arrow`}
+				onClick={onClick}
+			>
+				<svg className={`icon ${icon}`}>
+					<use href={`${sprite}#${icon}`} />
+				</svg>
+			</button>
+		);
+	};
+	const MainSlide = (props) => {
+		const { className, image, title, text } = props;
+		return (
+			<div className={className}>
+				<div
+					className="main__item"
+					style={{
+						backgroundImage: `url(${image})`,
+					}}
+				>
+					<div className="main__details">
+						<div className="main__live live live_big">Live</div>
+						<div className="main__title h4">{title || ""}</div>
+						<div className="main__parameters">
+							<div className="main__parameter">
+								<div className="main__icon">
+									<img
+										className="main__pic"
+										src={league}
+										alt=""
+									/>
+								</div>
+								<div className="main__text">{text || ""}</div>
+							</div>
+							<div className="main__parameter">🇺🇸 English</div>
+						</div>
+						<a className="main__btn btn btn_purple" href="/#">
+							Watch Now
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	};
+	const settings = {
+		dotsClass: "main__nav",
+		infinite: true,
+		nextArrow: (
+			<ButtonArrow className="slick-next" icon="icon-arrow-next" />
+		),
+		prevArrow: (
+			<ButtonArrow className="slick-prev" icon="icon-arrow-prev" />
+		),
+	};
+	return (
+		<div className="main__container">
+			<Slider
+				className="main__for"
+				{...settings}
+				asNavFor={nav2}
+				ref={(slider) => {
+					slider1 = slider;
+				}}
+			>
+				{list
+					? list.map((item) => {
+							return (
+								<MainSlide
+									className="main__slide"
+									image={item.image}
+									title={item.title}
+									text={item.text}
+								/>
+							);
+					  })
+					: null}
+			</Slider>
+			<Slider
+				className="main__nav"
+				{...settings}
+				asNavFor={nav1}
+				ref={(slider) => {
+					slider2 = slider;
+				}}
+				slidesToShow={4}
+				swipeToSlide="true"
+				focusOnSelect="true"
+			>
+				{list
+					? list.map((item) => {
+							return (
+								<div
+									className="main__preview"
+									style={{
+										backgroundImage: `url(${item.image})`,
+									}}
+								/>
+							);
+					  })
+					: null}
+			</Slider>
+		</div>
+	);
+};
+
+export default SliderComponent;
--- client/src/pages/AccountPage.js
+++ client/src/pages/AccountPage.js
@@ -1,172 +1,24 @@
-import React, { useEffect, useState } from "react";
-import Slider from "react-slick";
+import React, { useState } from "react";
 import sprite from "../assets/icons/sprite.svg";
-import mainPic1 from "../assets/image/main-pic-1.png";
-import mainPic2 from "../assets/image/main-pic-2.jpg";
-import mainPic3 from "../assets/image/main-pic-3.jpg";
-import mainPic4 from "../assets/image/main-pic-4.jpg";
-import league from "../assets/image/league-of-legends.png";
 import callOfDuty from "../assets/image/call-of-duty.png";
 import ava from "../assets/image/ava-1.png";
+import { SliderComponent } from "../components/_components";
 
 const AccountPage = () => {
-	const [nav1, setNav1] = useState(null);
-	const [nav2, setNav2] = useState(null);
-
-	let slider1 = [];
-	let slider2 = [];
-	useEffect(() => {
-		setNav1(slider1);
-		setNav2(slider2);
-	});
-	const list = [
-		{
-			image: mainPic1,
-			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
-			text: "League of Legends®",
-		},
-		{
-			image: mainPic2,
-			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
-			text: "League of Legends®",
-		},
-		{
-			image: mainPic3,
-			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
-			text: "League of Legends®",
-		},
-		{
-			image: mainPic4,
-			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
-			text: "League of Legends®",
-		},
-		{
-			image: mainPic2,
-			title: "Renegades vs Chiefs - ESL ProLeague Season 16",
-			text: "League of Legends®",
-		},
-	];
 	const [catalogNav, setCatalogNav] = useState(0);
 	const catalogsNav = ["Videos", "About", "Stories", "Schedules"];
 	const [catalogTag, setCatalogTag] = useState(0);
 	const catalogsTag = ["All", "Stream Videos", "Videos"];
-	const ButtonArrow = (props) => {
-		const { className, onClick, icon } = props;
-		return (
-			<button
-				type="button"
-				className={`${className} slick-arrow`}
-				onClick={onClick}
-			>
-				<svg className={`icon ${icon}`}>
-					<use href={`${sprite}#${icon}`} />
-				</svg>
-			</button>
-		);
-	};
-	const MainSlide = (props) => {
-		const { className, image, title, text } = props;
-		return (
-			<div className={className}>
-				<div
-					className="main__item"
-					style={{
-						backgroundImage: `url(${image})`,
-					}}
-				>
-					<div className="main__details">
-						<div className="main__live live live_big">Live</div>
-						<div className="main__title h4">{title || ""}</div>
-						<div className="main__parameters">
-							<div className="main__parameter">
-								<div className="main__icon">
-									<img
-										className="main__pic"
-										src={league}
-										alt=""
-									/>
-								</div>
-								<div className="main__text">{text || ""}</div>
-							</div>
-							<div className="main__parameter">🇺🇸 English</div>
-						</div>
-						<a className="main__btn btn btn_purple" href="/#">
-							Watch Now
-						</a>
-					</div>
-				</div>
-			</div>
-		);
-	};
-	const settings = {
-		dotsClass: "main__nav",
-		infinite: true,
-		nextArrow: (
-			<ButtonArrow className="slick-next" icon="icon-arrow-next" />
-		),
-		prevArrow: (
-			<ButtonArrow className="slick-prev" icon="icon-arrow-prev" />
-		),
-	};
+	
 	return (
 		<div className="main main_channel js-main">
-			<div className="main__container">
-				<Slider
-					className="main__for"
-					{...settings}
-					asNavFor={nav2}
-					ref={(slider) => {
-						slider1 = slider;
-					}}
-				>
-					{list
-						? list.map((item) => {
-								return (
-									<MainSlide
-										className="main__slide"
-										image={item.image}
-										title={item.title}
-										text={item.text}
-									/>
-								);
-						  })
-						: null}
-				</Slider>
-				<Slider
-					className="main__nav"
-					{...settings}
-					asNavFor={nav1}
-					ref={(slider) => {
-						slider2 = slider;
-					}}
-					slidesToShow={4}
-					swipeToSlide="true"
-					focusOnSelect="true"
-				>
-					{list
-						? list.map((item) => {
-								return (
-									<div
-										className="main__preview"
-										style={{
-											backgroundImage: `url(${item.image})`,
-										}}
-									/>
-								);
-						  })
-						: null}
-				</Slider>
-			</div>
+			<SliderComponent />
 			<div className="page__center page__center_pt0">
 				<div className="author author_big">
 					<div className="author__container">
 						<div className="author__details">
 							<div className="ava ava_online">
-								<img
-									className="ava__pic"
-									src={ava}
-									alt=""
-								/>
+								<img className="ava__pic" src={ava} alt="" />
 							</div>
 							<div className="author__wrap">
 								<div className="author__man h2 confirm">
@@ -214,6 +66,7 @@ const AccountPage = () => {
 													? "active"
 													: null
 											}`}
+											href="#"
 											onClick={() => setCatalogNav(index)}
 										>
 											{item}
@@ -237,7 +90,7 @@ const AccountPage = () => {
 								? catalogsTag.map((item, index) => {
 										return (
 											<a
-												className="catalog__tag active"
+												className="catalog__tag"
 												href="#"
 												onClick={() =>
 													setCatalogTag(index)
